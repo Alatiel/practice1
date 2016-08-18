4a5dbcde482e668e87ca674e304ae48143e3d20d
inna <NOfate281>
2016-08-18 10:21:13 +0300

    done

--- src/class1.java
+++ src/class1.java
@@ -1,3 +1,5 @@
+import apple.laf.JRSUIConstants;
+
 /**
  * Created by inna on 18/08/2016.
  */
@@ -7,18 +9,29 @@ public class class1 {
 
         int[] numbersArray = new int[]{5,4,3,2,1,2,3,4,5};
 
-        for (int repeat = 0;repeat<numbersArray.length; repeat++){
+        for (int repeat = 0;repeat<numbersArray.length; repeat++){ //for (int r : numbersArray)
 
-            for (int i = 0; i<numbersArray.length-1; i++) {
+            for (int i = 0; i<numbersArray.length-1; i=i+1) {
 
-                if (numbersArray[i] < numbersArray[i]) {
+                if (numbersArray[i] < numbersArray[i+1]) {
                     int temp = numbersArray[i];
                     numbersArray[i] = numbersArray[i + 1];
                     numbersArray[i + 1] = temp;
                 }
             }
-            System.out.println();
+            System.out.println("repeat " + repeat);
+
+            showArray(numbersArray);
+        }
+    }
+    public static void showArray(int[] array){
+
+        System.out.println();
+
+        for(int r:array){
+            System.out.print(r+",");
         }
 
+        System.out.println();
     }
 }
